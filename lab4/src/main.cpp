efe14ca02ec69386f68a51c326422540c95f75a7
Uzym <lyutoev2003ia@gmail.com>
2022-12-03 10:18:03 +0300

    res

--- lab4/src/main.cpp
+++ lab4/src/main.cpp
@@ -1,6 +1,7 @@
 //
 // Created by lyutoev on 27.10.22.
 //
+
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
@@ -8,102 +9,76 @@
 #include <semaphore.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <string.h>
 
-#include "../include/main.h"
-
-using namespace std;
-
-struct data {
-    int sum;
-    int st;
-};
-
-int sem_get(sem_t *sem) {
-    int val;
-    sem_getvalue(sem, &val);
-    return val;
-}
-
-void sem_set_value(sem_t *sem, int n)
+int main(int argc, char const *argv[])
 {
-    while(sem_get(sem) < n) {
-        sem_post(sem);
-    }
-    while(sem_get(sem) > n) {
-        sem_wait(sem);
-    }
-}
-
-int main() {
-    data* map = (data*)mmap(0, sizeof(data), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
+    char* map = (char*) mmap(0, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
     if (map == MAP_FAILED) {
-        cerr << "mmap failed\n";
+        std::cerr << "mmap failed\n";
+        return 1;
+    }
+    std::string fileName;
+    std::cout << "Input file path:\n";
+    std::getline(std::cin, fileName);
+    int fd = open(fileName.c_str(), O_RDONLY);
+    if (fd == -1) {
+        std::cerr << "file error\n";
         return 1;
     }
     sem_unlink("_sem");
-    sem_t *sem = sem_open("_sem", O_CREAT, 0, 2);
-
-    cout << "Input file path: ";
-    string file_name;
-    getline(cin, file_name);
-
-    int file = open(file_name.c_str(), O_RDONLY);
-    if (file == -1) {
-        cerr << "file error\n";
-        return 1;
+    sem_t *sem = sem_open("_sem", O_CREAT, 0, 1);
+    int state = 0;
+    while (state++ < 2) { // 1
+        sem_post(sem);
     }
-    sem_set_value(sem, SEM_WAIT);
-    map->st = 0;
-
-    pid_t fork_id = fork();
-    if (fork_id == -1) {
-        cerr << "fork error\n";
-        return 1;
-    } else if (fork_id == 0) {
-
-        if (dup2(file, 0) == -1) {
+    while (state-- > 1) {
+        sem_wait(sem);
+    }
+    pid_t id = fork();
+    if (id == 0) {
+        if (dup2(fd, 0) == -1) {
             std::cerr << "dub error\n";
             return -1;
         }
+        int sum = 0, x;
         char c;
-        int x, sum = 0;
-        do {
-            while (map->st != 0);
-            if (sem_get(sem) == SEM_GET) {
-                continue;
-            }
+        while (1) {
             std::cin >> x;
             sum += x;
             c = getchar();
-            if (c == '\n') {
-                map->sum = sum;
-                map->st = 1;
-                if (sem_get(sem) == SEM_WAIT) {
-                    sem_set_value(sem, SEM_GET);
-                }
-                sum = 0;
+            if (c == EOF) {
+                sem_post(sem);
+                sem_post(sem);
+                sem_post(sem);
+                break;
             }
-        } while(c != EOF);
-        sem_set_value(sem, SEM_EXIT);
-        map->st = 2;
-        close(file);
-
-    } else {
-        while (sem_get(sem) != SEM_EXIT && map->st != 2) {
-            while (map->st == 0);
-            if (sem_get(sem) == SEM_WAIT) {
-                continue;
-            } else if (sem_get(sem) == SEM_GET) {
-                cout << map->sum << "\n";
-                map->st = 0;
-                sem_set_value(sem, SEM_WAIT);
+            if (c == '\n') {
+                memcpy(map, &sum, sizeof(int));
+                sum = 0;
+                sem_wait(sem); // 0
+                sem_wait(sem);
             }
         }
+        munmap(map, sizeof(int));
+        sem_close(sem);
+        sem_destroy(sem);
+    } else {
+        do {
+            sem_getvalue(sem, &state);
+            if (state == 0) {
+                int *allocated = (int *) malloc(sizeof(int));
+                memcpy(allocated, map, sizeof(int));
+                std::cout << *allocated << "\n";
+                free(allocated);
+                sem_post(sem);
+                sem_post(sem);
+            }
+        } while (state <= 3);
+        munmap(map, sizeof(int));
+        sem_close(sem);
+        sem_destroy(sem);
     }
-    munmap(map, sizeof(data));
-    sem_close(sem);
-    sem_destroy(sem);
 
     return 0;
-
 }
